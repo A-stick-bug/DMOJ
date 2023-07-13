0aa4886a9f387205cb68916603b8bf4cc5de6548
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-07-13 13:19:45 -0400

    simplified code

--- DMOPC '16 Contest 4 P2 - Systests.py
+++ DMOPC '16 Contest 4 P2 - Systests.py
@@ -5,20 +5,17 @@ from bisect import bisect_left, bisect_right
 n_batches = int(input())
 
 batches = []  # (start, end, point)
-max_points = 0
 for i in range(n_batches):
     start, end, points = map(int, input().split())
     batches.append((start, end, points))
-    max_points += points  # instead of deducting from max possible, we can just start at 0 and add the batches that passed
 
 n_wrong = int(input())
 wrong = [int(input()) for _ in range(n_wrong)]
 wrong.sort()
 
+res = 0
 for start, end, points in batches:
-    cut_start = bisect_left(wrong, start)
-    cut_end = bisect_right(wrong, end)
-    if cut_start != cut_end:  # there is a wrong test case in this range
-        max_points -= points  # deduct points from this batch
+    if bisect_left(wrong, start) == bisect_right(wrong, end):  # all cases in this batch passes
+        res += points  # deduct points from this batch
 
-print(max_points)
+print(res)
