4efaf0e2f566ad1309a839ea598caa236db21d52
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-07-16 18:24:24 -0400

    improved code

--- DMOPC '21 Contest 7 P2 - Knitting Scarves.py
+++ DMOPC '21 Contest 7 P2 - Knitting Scarves.py
@@ -1,33 +1,63 @@
 # very weird graph theory
 # oops, it's actually supposed to be a doubly linked list
 
-def print_graph():
-    i = 0  # whatever 0 points to will be the root/head/start node
-    res = []
-    for _ in range(n):
-        i = graph[i][1]
-        res.append(str(i))
-    return " ".join(res)
-
-
-n, steps = map(int, input().split())
-graph = [[i - 1, i + 1] for i in range(n + 2)]
+nodes, steps = map(int, input().split())
+before = [i - 1 for i in range(nodes + 2)]
+next = [i + 1 for i in range(nodes + 1)]
 
 for i in range(steps):
     start, end, cut = map(int, input().split())
 
     # fill in the gap
-    prev = graph[start][0]
-    graph[prev][1] = graph[end][1]
-    next = graph[end][1]
-    graph[next][0] = graph[start][0]
+    previous = before[start]
+    next[previous] = next[end]
+    temp = next[end]
+    before[temp] = before[start]
 
     # fix connection after insertion
-    next = graph[cut][1]
-    graph[next][0] = end
-    graph[end][1] = next
+    temp = next[cut]
+    before[temp] = end
+    next[end] = temp
 
-    graph[cut][1] = start
-    graph[start][0] = cut
+    next[cut] = start
+    before[start] = cut
 
-print(print_graph())
+i = 0  # whatever 0 points to will be the root/head/start node
+res = []
+for _ in range(nodes):
+    i = next[i]
+    res.append(str(i))
+print(*res, sep=" ")
+
+# # bad code that almost TLE and MLE and only works in PYPY3
+#
+# def print_graph():
+#     i = 0  # whatever 0 points to will be the root/head/start node
+#     res = []
+#     for _ in range(n):
+#         i = graph[i][1]
+#         res.append(str(i))
+#     return " ".join(res)
+#
+#
+# n, steps = map(int, input().split())
+# graph = [[i - 1, i + 1] for i in range(n + 2)]
+#
+# for i in range(steps):
+#     start, end, cut = map(int, input().split())
+#
+#     # fill in the gap
+#     prev = graph[start][0]
+#     graph[prev][1] = graph[end][1]
+#     next = graph[end][1]
+#     graph[next][0] = graph[start][0]
+#
+#     # fix connection after insertion
+#     next = graph[cut][1]
+#     graph[next][0] = end
+#     graph[end][1] = next
+#
+#     graph[cut][1] = start
+#     graph[start][0] = cut
+#
+# print(print_graph())
