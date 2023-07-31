ca3028fb0ffec3c19bc81a508af0f28ec5d24273
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-07-30 21:35:38 -0400

    Create Alice's Shopping Trip.py

---
+++ Alice's Shopping Trip.py
@@ -0,0 +1,27 @@
+time, n = map(int, input().split())
+stores = [int(input()) for _ in range(n)]
+stores.sort(key=lambda x: abs(x))
+
+res = 0  # number of stores visited
+cur = 0  # current location
+
+for i, loc in enumerate(stores):
+    dist = abs(cur - loc)  # distance from current location to next
+    if time - dist < 0:  # check if we are out of time
+        break
+
+    cur = loc  # update to new location
+    time -= dist
+    res += 1
+
+print(res)
+
+# test case:
+# 25 5
+# 10
+# -3
+# 8
+# -7
+# 1
+
+# answer: 4
