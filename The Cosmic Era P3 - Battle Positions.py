e38185b1dc4e94032e4c959e316413d45088ddce
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-08-06 17:00:47 -0400

    added comments

--- The Cosmic Era P3 - Battle Positions.py
+++ The Cosmic Era P3 - Battle Positions.py
@@ -1,22 +1,23 @@
+# https://dmoj.ca/problem/seed3
+
 import sys
 
 input = sys.stdin.readline
-
 n_stations = int(input())
 min_troops = int(input())
 n_waves = int(input())
 
-add_troops = [0 for _ in range(n_stations+2)]
+add_troops = [0 for _ in range(n_stations + 2)]  # +2 for 1-indexing and preventing index out of bounds
 
 for _ in range(n_waves):
     start, end, amount = map(int, input().split())
-    add_troops[start] += amount
-    add_troops[end+1] -= amount
+    add_troops[start] += amount  # +amount troops for stations up to end
+    add_troops[end + 1] -= amount  # once we are out of this wave, the troops will not be added anymore
 
 res = 0
-troops = 0
-for i in range(1,n_stations+1):
-    troops += add_troops[i]
+troops = 0  # how many troops are in the current station
+for i in range(1, n_stations + 1):
+    troops += add_troops[i]  # update the amount of troops at this station
     # print(troops)
     if troops < min_troops:
         res += 1
