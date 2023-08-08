d837f43199de59e0455baeda96b6e8f32e1e6e7d
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-08-08 09:03:03 -0400

    sliding window

--- Another Contest 3 Problem 1 - Diverse Arrays.py
+++ Another Contest 3 Problem 1 - Diverse Arrays.py
@@ -1,7 +1,7 @@
-# TLE, check c++ solution
+# https://dmoj.ca/problem/acc3p1
+# using a frequency array and sliding window, and some weird subarray formula
 
 import sys
-from collections import defaultdict
 
 input = sys.stdin.readline  # need this, up to 1 mil lines of input
 
@@ -10,16 +10,19 @@ nums = [int(input()) for _ in range(n)]
 
 total = (n * (n + 1)) // 2  # total number of sub arrays
 
-freq = defaultdict(int)
+freq = [0] * (n + 1)  # this is faster than a dictionary (and defaultdict)
+unique = 0  # unique elements in current window
 left = 0  # sliding window
 for right in range(n):
+    if freq[nums[right]] == 0:  # element not in window
+        unique += 1
     freq[nums[right]] += 1
 
-    while len(freq) >= k:
+    while unique >= k:  # move pointer until there is less than K unique elements
         left_num = nums[left]
         freq[left_num] -= 1
-        if freq[left_num] == 0:
-            del freq[left_num]
+        if freq[left_num] == 0:  # element moved out of window
+            unique -= 1
         left += 1
 
     total -= (right - left + 1)
