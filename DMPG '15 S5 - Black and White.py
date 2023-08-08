7c8b6bcd36a478d0a3b40c7c6724563822067ad3
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-08-08 19:14:15 -0400

    full solution

--- DMPG '15 S5 - Black and White.py
+++ DMPG '15 S5 - Black and White.py
@@ -1,21 +1,51 @@
-# using bitwise operators, MLE
+# https://dmoj.ca/problem/dmpg15s5
+# using a difference array (1 row at a time)
+
+import sys
+
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
-dif = [[False for _ in range(n + 2)] for _ in range(n + 2)]
-
+rect = [[] for _ in range(n + 1)]  # a 2D array of bools takes too much memory
 for _ in range(m):
     c, r, w, h = map(int, input().split())
-    r += 1
-    c += 1
-    dif[r][c] ^= True  # inverse bit, equivalent to not arr[y][x]
-    dif[r][c + w] ^= True
-    dif[r + h][c] ^= True
-    dif[r + h][c + w] ^= True
+    rect[r].append(c)  # note the places where the difference changes
+    rect[r].append(c + w)
+    rect[r + h].append(c)
+    rect[r + h].append(c + w)
 
-count = 0
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        dif[i][j] ^= dif[i - 1][j] ^ dif[i][j - 1] ^ dif[i - 1][j - 1]
-        count += dif[i][j]
+dif = [0 for _ in range(n + 1)]  # do 1 row at a time to save memory
+res = 0
+for i in range(n):
+    for j in rect[i]:  # build the difference array for this row
+        dif[j] ^= True
 
-print(count)
+    cur = False
+    for j in range(n):  # count black tiles
+        cur ^= dif[j]
+        res += cur
+
+print(res)
+
+
+# # TLE, O(mn + n^2), optimized code above
+# import sys
+#
+# input = sys.stdin.readline
+#
+# n, m = map(int, input().split())
+# diff = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+# for _ in range(m):
+#     c, r, w, h = map(int, input().split())
+#     for i in range(r, r +h):
+#         diff[i][c] ^= True
+#         diff[i][c+w] ^= True
+#
+# res = 0
+# for i in range(n):
+#     state = 0
+#     for j in range(n):
+#         state ^= diff[i][j]
+#         res += state
+#
+# print(res)
