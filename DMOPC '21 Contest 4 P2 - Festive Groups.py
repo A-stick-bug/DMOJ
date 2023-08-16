f3274ec8c9ddefb359df8422e6871daf083ffa9b
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-08-16 14:54:09 -0400

    counting sort and sieve

---
+++ DMOPC '21 Contest 4 P2 - Festive Groups.py
@@ -0,0 +1,19 @@
+# https://dmoj.ca/problem/dmopc21c4p2
+# use counting sort but ignore duplicate values
+
+_ = int(input())
+nums = [int(x) for x in input().split()]
+
+groups = 0
+n = 5_000_002
+multiples = [False] * n
+for i in nums:
+    multiples[i] = True
+
+for num in range(1, n):
+    if multiples[num]:
+        groups += 1
+        for i in range(num, n, num):
+            multiples[i] = False
+
+print(groups)
