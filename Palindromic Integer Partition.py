b3b85cced76fb64d64459b54db4cc28902260221
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-08-20 22:10:08 -0400

    combinatorics OR recursion/memo

---
+++ Palindromic Integer Partition.py
@@ -0,0 +1,29 @@
+# O(n) solution using combinatorics
+
+n = int(input())
+total = 0
+partition = lambda x: 2 ** (x - 1)
+
+if n % 2 == 1:  # for odd, the middle is 1 number, n-mid must be even so that both sides can be made equal
+    for i in range(1, n - 1, 2):
+        total += partition((n - i) // 2)
+
+else:  # for even numbers, the middle can be 1 number or be split into 2 numbers
+    for i in range(2, n - 1, 2):
+        total += partition((n - i) // 2) * 2  # therefore, we multiply the partitions by 2
+
+    total += 1  # special case of (n/2, n/2), occurs on all even numbers
+
+print(total + 1)  # +1 because n itself counts
+
+
+# using recursion + memoization
+# from functools import cache
+#
+#
+# @cache
+# def partition(n):
+#     return sum(partition(n - mid * 2) for mid in range(1, n // 2 + 1)) + 1
+#
+#
+# print(partition(int(input())))
