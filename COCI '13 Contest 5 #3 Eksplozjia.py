ab981c18e6c4a00ecb87a10b3754ac3174290fcd
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-08-23 09:51:51 -0400

    added full solution

--- COCI '13 Contest 5 #3 Eksplozjia.py
+++ COCI '13 Contest 5 #3 Eksplozjia.py
@@ -1,42 +1,18 @@
-# MLE, 5/15
-# quite similar to the valid parenthesis question (but the second string has length > 2)
-# "The explosion will not contain two equal characters" makes it a lot easier
+# O(N*M), where n is the length of the string and m is the length of the bomb
+
 
 s = input()
 bomb = input()
-bomb_letters = set(bomb)
+nb = len(bomb)
+lb = list(bomb)
+
 stack = []
-deleted = set()  # contains indices of all deleted letters
-full_bomb = True
+for char in s:
+    stack.append(char)
 
-if len(bomb) == 1:  # corner case
-    s = s.replace(bomb, "")
+    # trying to check for a match with bomb on every iteration is fine because the bomb is so small
+    if len(stack) >= nb and stack[len(stack) - nb:] == lb:
+        for i in range(nb):
+            stack.pop()
 
-for i, letter in enumerate(s):
-    if letter not in bomb_letters:  # basically prevents any previous possible explosions
-        stack.clear()
-        full_bomb = False
-        continue
-
-    if letter == bomb[0]:  # start of a bomb
-        stack.append((i, 0))  # index of character, index in bomb, letter
-        full_bomb = True
-
-    elif stack:
-        bomb_index = stack[-1][1]
-        if letter != bomb[bomb_index + 1]:  # doesn't match next letter in bomb
-            stack.clear()
-            full_bomb = False
-
-        elif bomb_index == len(bomb) - 2 and full_bomb:  # matches next letter and is the last letter
-            deleted.add(i)
-            for _ in range(len(bomb) - 1):
-                deleted.add(stack.pop()[0])
-
-        else:  # matches same next letter in bomb, not the last one
-            stack.append((i, bomb_index + 1))
-
-if not s or len(deleted)==len(s):
-    print("FRULA")
-else:
-    print(*[char if i not in deleted else "" for i, char in enumerate(s)], sep="")
+print('FRULA') if not stack else print(''.join(stack))
