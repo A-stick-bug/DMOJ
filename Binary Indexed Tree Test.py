520d30d32c41c9ee9e5b36c9820adf82a0badaea
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-09-11 17:21:20 -0400

    added comments

--- Binary Indexed Tree Test.py
+++ Binary Indexed Tree Test.py
@@ -1,3 +1,4 @@
+# standard fenwick tree operations, nothing special here
 def update(x, a, v):
     while x < len(a):
         a[x] += v
@@ -22,6 +23,8 @@ freq = [0] * 100001
 for i in range(1, n + 1):
     var = arr[i - 1]
     update(i, bit, var)
+    # we need a BIT for the frequency array to answer the
+    # how many elements are less than or equal to v in the array queries
     update(var, freq, 1)
 
 for _ in range(queries):
@@ -29,16 +32,16 @@ for _ in range(queries):
     q = line[0]
     if q == 'C':
         a, b = int(line[1]), int(line[2])
-        v = arr[a - 1]
-        update(a, bit, b - v)
-        update(v, freq, -1)
-        update(b, freq, 1)
-        arr[a - 1] = b
+        v = arr[a - 1]  # element to be changed
+        update(a, bit, b - v)  # adding (new_element - previous_element) will turn the previous element to b
+        update(v, freq, -1)  # -1 for the element that got changed
+        update(b, freq, 1)  # +1 for the new element
+        arr[a - 1] = b  # update out original array
 
     elif q == 'S':
         a, b = int(line[1]), int(line[2])
-        print(query(b, bit) - query(a - 1, bit))
+        print(query(b, bit) - query(a - 1, bit))  # query is same as prefix sum
 
     else:
         a = int(line[1])
-        print(query(a, freq))
+        print(query(a, freq))  # querying the frequency array will give us the number of elements less than v
