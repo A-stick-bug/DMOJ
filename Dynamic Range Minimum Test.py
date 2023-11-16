f616765cf885c5ac859ed489d93ac8417b3e88df
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-11-16 12:10:10 -0500

    added seg tree sol (slower)

--- Dynamic Range Minimum Test.py
+++ Dynamic Range Minimum Test.py
@@ -56,3 +56,61 @@ for _ in range(Q):
         sq.update(i, j)
     else:
         print(sq.get_range(i, j))
+
+# # boring way using segment tree
+# # root is at 1, children of node i is seg[i*2] and seg[i*2+1], parent of node i is seg[i//2]
+#
+# import sys
+# from math import log2, ceil
+#
+#
+# # create segment tree from array
+# def create() -> None:
+#     p2 = 1 << layers
+#     for i in range(N):  # base layer
+#         seg[p2 + i] = arr[i]
+#     for i in reversed(range(1, p2)):  # create other layers from base
+#         seg[i] = min(seg[i * 2], seg[i * 2 + 1])
+#
+#
+# # update i-th element to val, 0-indexed
+# def update(i, val) -> None:
+#     i += 1 << layers  # start from bottom
+#     seg[i] = val
+#     while i > 1:
+#         i //= 2
+#         seg[i] = min(seg[i * 2], seg[i * 2 + 1])
+#
+#
+# # query left to right inclusive
+# def query(i, l, r, tl, tr) -> int:
+#     if l > r:
+#         return inf
+#     if l == tl and r == tr:
+#         return seg[i]
+#     tm = (tl + tr) // 2
+#     return min(query(i * 2, l, min(r, tm), tl, tm),
+#                query(i * 2 + 1, max(l, tm + 1), r, tm + 1, tr))
+#
+#
+# input = sys.stdin.readline
+# inf = 1 << 30
+#
+# N, Q = map(int, input().split())
+# arr = [int(input()) for _ in range(N)]
+#
+# layers = ceil(log2(N))  # max depth of the seg tree, root has depth 0
+# seg = [inf] * (1 << (layers + 1))  # 1-indexed, need an extra layer for the root
+# create()
+# N = 1 << layers
+# # print(seg)
+#
+# for _ in range(Q):
+#     q = input().split()
+#     if q[0] == "M":
+#         i, v = map(int, q[1:])
+#         update(i, v)
+#     else:
+#         l, r = map(int, q[1:])
+#         print(query(1, l, r, 0, N - 1))
+#     # print(seg)
