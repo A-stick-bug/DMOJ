f7b37c4d6b4911bade04b95ce97019a52ec5765d
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-12-05 08:25:07 -0500

    accidently put wrong code

--- COCI '22 Contest 4 #5 Vrsta.py
+++ COCI '22 Contest 4 #5 Vrsta.py
@@ -1,6 +1,12 @@
-# using order statistics tree to get median
+# https://dmoj.ca/problem/coci22c4p5
+# get median using an order statistic tree (implemented using Fenwick Tree)
+# this has a better constant factor (therefore faster) than a segment tree
 
-class Better_OST:
+from math import ceil
+import sys
+
+
+class OST:
     def __init__(self, size):
         self.size = size
         self.tree = [0] * (size + 1)
@@ -26,3 +32,20 @@ class Better_OST:
                 k -= self.tree[i]
         return i + 1
 
+
+input = sys.stdin.readline
+n = int(input())
+queries = [list(map(int, input().split())) for _ in range(n)]
+
+# coordinate compression
+decompress = sorted(set(list(map(lambda x: x[0], queries))))
+compress = {val: i + 1 for i, val in enumerate(decompress)}
+
+ost = OST(len(decompress) + 1)  # have enough spots for every height
+people = 0
+for a, b in queries:
+    a = compress[a]
+    ost.update(a, b)  # b people with a height get added
+    people += b
+
+    print(decompress[ost.select(ceil(people / 2)) - 1])  # decompress to get original value
