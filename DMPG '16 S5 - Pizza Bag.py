287199fb9cf26f504ed9967669e3b952fa5d87de
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-01-28 11:39:57 -0500

    rewrote psa+deque solution

--- DMPG '16 S5 - Pizza Bag.py
+++ DMPG '16 S5 - Pizza Bag.py
@@ -1,25 +1,28 @@
-# using prefix sum and monotonic queue
+# https://dmoj.ca/problem/dmpg16s5
+# same strategy as https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/
+# Using prefix sum array (PSA) and monotonic queue
+#
+# find maximum subarray sum with length less than K
+# we can use a decreasing monotonic queue that stores (index, sum up to index) so we can remove everything up to index
+# since it is better to have a greater sum, we want to remove the minimum sum possible, while keeping the length (< K)
 
 from collections import deque
+from itertools import accumulate
 
-n, m = map(int, input().split())
-slices = list(map(int, input().split()))
-slices.extend(slices)  # add an extra copy of it at the end because it is circular
+N, K = map(int, input().split())
+arr = [0] + list(map(int, input().split())) * 2  # double array as it is circular
+psa = tuple(accumulate(arr))
 
-prefix_sum = [0] * (2 * n + 1)
-for i in range(1, 2 * n + 1):
-    prefix_sum.append(prefix_sum[i - 1] + slices[i - 1])
+best = 0
+q = deque([0])  # add the option to remove nothing
+for i in range(1, 2 * N + 1):
+    best = max(best, psa[i] - psa[q[0]])  # remove the minimum possible slice option (so we keep the most)
 
-res = 0  # max deliciousness obtained
-q = deque()
-
-for i in range(1, 2 * n + 1):
-    while q and i - q[0] > m:
-        q.popleft()
-    if q:
-        res = max(res, prefix_sum[i] - prefix_sum[q[0]])
-    while q and prefix_sum[q[-1]] >= prefix_sum[i]:
+    while q and psa[q[-1]] >= psa[i]:  # add current sum as an option to remove, only keep smaller sums
         q.pop()
     q.append(i)
 
-print(res)
+    if i - q[0] == K:  # slice is too big, move window
+        q.popleft()
+
+print(best)
