b9479b690e2a69096a476d6e3a30e66ecf209a9d
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-02-06 20:06:49 -0500

    2 changes
    1. psa + binary search
    2. alternate method to fill in interval DP table

--- Educational DP Contest AtCoder N - Slimes.py
+++ Educational DP Contest AtCoder N - Slimes.py
@@ -1,6 +1,8 @@
 # https://dmoj.ca/problem/dpn
 # template interval DP, using PSA to speed things up
-# the hard part is finding out how to do the state transition
+#
+# Transition: loop from smaller to larger intervals, the current interval can be created from 2 smaller ones,
+#             try all possible split point (in other words try all pairs of intervals on the left and right)
 # TC: O(N^3)
 
 from itertools import accumulate
@@ -18,9 +20,11 @@ def solve():
     for i in range(n):  # base case: single elements are already merged
         dp[i][i] = 0
 
-    for i in reversed(range(n)):
-        for j in range(i + 1, n):
-            for s in range(i, j):  # combine: [l, s] + [s+1, r] -> [l, r]
+    for length in range(n):  # 0-indexed length, true length of current [i,j] is actually 1 more
+        for i in range(n - length):
+            j = i + length
+            for s in range(i, j):  # find best way to combine: [l, s] + [s+1, r] -> [l, r]
+                # these segments are guaranteed to exist already since they are smaller
                 dp[i][j] = min(dp[i][j], dp[i][s] + dp[s + 1][j] + query(i, j))
 
     print(dp[0][n - 1])
