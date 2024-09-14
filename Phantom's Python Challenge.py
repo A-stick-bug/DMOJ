2b0fe0a37c7b74e92b7e7abb610057b87cd0966b
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-09-14 09:22:09 -0400

    shorten

--- Phantom's Python Challenge.py
+++ Phantom's Python Challenge.py
@@ -1,4 +1,4 @@
-# 20/40, code is still too long
+# 28/40, code is still too long
 # strategy: first write a working code, then improve from there
 
 # # base code
@@ -30,4 +30,4 @@
 #[n:=len(t:=[0]+[1]*int(input()))],[t.__setitem__(slice(p*p,n,p),[0]*((n-p*p+p-1)//p))for p in range(2,int(n**.5)+1)if t[p]],[print(str(p)+"*"*(t[p-2]|t[p+2]))for p in range(2,n)if t[p]]
 
 # checkpoint 3 (small optimizations)
-[n:=len(t:=[0]+[1]*int(input()))],[t.__setitem__(slice(p+p,n,p),[0]*((n-p-1)//p))for p in range(2,n)if t[p]],[print(str(p)+"*"*(t[p-2]|t[p+2]))for p in range(2,n)if t[p]]
+[t:=[0]+[1]*(n:=int(input()))],[t.__setitem__(slice(p+p,n,p),[0]*((n-p-1)//p))for p in range(2,n)if t[p]],[print(str(p)+"*"*(t[p-2]|t[p+2]))for p in range(2,n)if t[p]]
