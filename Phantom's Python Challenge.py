1fd21918891597643b95ff290a9ef4534384eb5d
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2024-11-16 10:45:18 -0500

    optimize

--- Phantom's Python Challenge.py
+++ Phantom's Python Challenge.py
@@ -30,4 +30,6 @@
 #[n:=len(t:=[0]+[1]*int(input()))],[t.__setitem__(slice(p*p,n,p),[0]*((n-p*p+p-1)//p))for p in range(2,int(n**.5)+1)if t[p]],[print(str(p)+"*"*(t[p-2]|t[p+2]))for p in range(2,n)if t[p]]
 
 # checkpoint 3 (small optimizations)
-[t:=[0]+[1]*(n:=int(input()))],[t.__setitem__(slice(p+p,n,p),[0]*((n-p-1)//p))for p in range(2,n)if t[p]],[print(str(p)+"*"*(t[p-2]|t[p+2]))for p in range(2,n)if t[p]]
+#[t:=[0]+[1]*(n:=int(input()))],[t.__setitem__(slice(p+p,n,p),[0]*((n-p-1)//p))for p in range(2,n)if t[p]],[print(str(p)+"*"*(t[p-2]|t[p+2]))for p in range(2,n)if t[p]]
+
+[t:=[0]+[1]*(n:=int(input())),r:=range],[t.__setitem__(i,0)for p in r(2,n)if t[p]for i in r(p*p,n,p)],[print(str(p)+"*"*(t[p-2]|t[p+2]))for p in r(2,n)if t[p]]
