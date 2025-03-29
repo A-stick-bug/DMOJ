90d35aca520ee36da7aefe5db9cd18bf28e16788
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2025-03-28 21:02:54 -0400

    added hashing sol

--- MNYC '17 - Hurontario.py
+++ MNYC '17 - Hurontario.py
@@ -53,3 +53,57 @@ for i in range(m + 1, n + m + 1):
 
 # no match
 print(s + t)
+
+# # alternate hashing solution
+# # basically brute force optimized with hashing for O(1) substring query
+#
+# import sys
+#
+# # max possible length of anything string
+# # all hashes are aligned with this maximum length
+# MN = 10 ** 6
+#
+#
+# class SubstringHash:
+#     def __init__(self, arr, p=29, mod=10 ** 9 + 7):
+#         # precompute powers of `p`, with MOD
+#         self.mod = mod
+#         self.power = [0] * MN
+#         self.power[0] = 1
+#         for i in range(1, MN):
+#             self.power[i] = (self.power[i - 1] * p) % self.mod
+#
+#         # precompute hashes of each character in `str1`
+#         n = len(arr)
+#         self.psa = [0] * (n + 1)
+#         for i in range(1, n + 1):
+#             self.psa[i] = (arr[i - 1] * self.power[MN - i] + self.psa[i - 1]) % self.mod
+#
+#     def query(self, l, r):  # query hash of [l,r]
+#         # shift up to match `mn`
+#         hs = (self.psa[r + 1] - self.psa[l]) * self.power[l] % self.mod
+#         return hs
+#
+#
+# s, t = input().split()
+# n, m = len(s), len(t)
+#
+# ss = [ord(i) - ord("A") for i in s]
+# tt = [ord(i) - ord("A") for i in t]
+# s_hash = SubstringHash(ss)
+# # s_hash2 = SubstringHash(ss, mod=1000000009)
+# t_hash = SubstringHash(tt)
+# # t_hash2 = SubstringHash(tt, mod=1000000009)
+#
+# for le in reversed(range(1, min(n, m) + 1)):  # larger possible matches first
+#     # method 1: first check hash, then confirm by checking actual string
+#     # this works since as soon as we have an actual match, we break and there are little false positives
+#     if s_hash.query(n - le, n - 1) == t_hash.query(0, le - 1) and s[n - le:n] == t[:le]:
+#         print(s + t[le:])
+#         sys.exit()
+#
+#     # # method 2: double hash (slower in this case)
+#     # if (s_hash.query(n - le, n - 1) == t_hash.query(0, le - 1) and
+#     #         s_hash2.query(n - le, n - 1) == t_hash2.query(0, le - 1)):
+#
+# print(s + t)
